6e93f3be703ac9188ef183c3f30fc814a67f97d7
Thomas Dutrion <thomas@engineor.com>
2016-04-15 09:16:19 +0100

    Add Google API key

--- lecture/25--Angular/demo/03/script.js
+++ lecture/25--Angular/demo/03/script.js
@@ -2,7 +2,7 @@ var phonecatApp = angular.module('myApp', []);
 
 phonecatApp.controller('eventsCtrl', function ($scope, $http) {
     $scope.events = [];
-    var url = 'https://www.googleapis.com/calendar/v3/calendars/j0js43u21s8tu9j2s1q9bhhrtk%40group.calendar.google.com/events?key=';
+    var url = 'https://www.googleapis.com/calendar/v3/calendars/j0js43u21s8tu9j2s1q9bhhrtk%40group.calendar.google.com/events?key=AIzaSyAqx_E_V-2BQbKFP7o49KPYsGuCq4-WXdA';
     $http.get(url)
         .success(function(data) {
             $scope.events = data.items;
